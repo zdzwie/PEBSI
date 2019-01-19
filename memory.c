64543f8ae4d65a4450eca1a37786b754ef30a52c
Jakub Brzezowski <44787968+zdzwie@users.noreply.github.com>
2019-01-19 19:42:39 +0100

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,6 +1,29 @@
 /**
  ******************************Copyright(c)*************************
- *****************Jakub Brzezowski&Bart�omiej Bana�*****************
+ *****************Jakub Brzezowski&Bart³omiej Banaœ*****************
+ **
+ **----------------------------File info----------------------------
+ **
+ **
+ ** File name:               memory.c
+ ** Descriptions:            Flash memory driver.
+ **-----------------------------------------------------------------
+ ** Created by:              Jakub Brzezowski
+ ** Created date:            2019-01-15
+ ** Version:                 v1.5
+ ** Descriptions:            Library uses StPeriph.
+ ** Brief:					 First version of these library uses HAL library,
+ ** 						 but code aren't effective and during testing have problem with real time operating.
+ ** ---------------------------------------------------------------
+ ** Modified by:
+ ** Modified date:
+ ** Version:
+ ** Descriptions:
+ **
+ */
+/**
+ ******************************Copyright(c)*************************
+ *****************Jakub Brzezowski&Bartłomiej Banaś*****************
  **
  **----------------------------File info----------------------------
  **
@@ -24,72 +47,63 @@
 #include "memory.h"
 //-------------------------Functions Code------------------------------
 
-/**
- * @brief Setting adres of register, where data was written.
- * @note Data adress is four-bytes.
- * @author Jakub Brzezowski
- * @version 1.2 15-01-2019
- * @param addr is address, where data was written.
- * @retval none
- */
-void eeprom_set_addr(uint32_t addr)
-{
- I2C_GenerateSTART(I2C1, ENABLE);
- while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS);
- I2C_Send7bitAddress(I2C1, 0xa0, I2C_Direction_Transmitter);
- while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) != SUCCESS);
+void data_from_pc_read(uint8_t* buffer) {
 
- I2C_SendData(I2C1, addr);
- while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
+	USART_Configuration();
+
+	uint8_t len = sizeof(buffer) / sizeof(buffer[0]);
+	uint8_t i = 0;
+	uint8_t c;
+
+	while (i < (len - 1)) {
+		i++;
+		c = USART_receive_data();
+		if (c != NULL) {
+			buffer[i] = c;
+		} else {
+			i--;
+		}
+	}
 }
 
-/**
- * @brief Write data in memory.
- * @author Jakub Brzezowski
- * @version 1.2 15-01-2019
- * @param addr is address, where data was written.
- * @param data is data to save in memomry.
- * @param size is size of data.
- * @retval none
- */
-void eeprom_write(uint32_t addr, const void* data, int size)
-{
- int i;
- const uint8_t* buffer = (uint8_t*)data;
+NAND_ADDRESS write_read_address_set(uint16_t zone, uint16_t block, uint16_t page) {
 
- eeprom_set_addr(addr);
- for (i = 0; i < size; i++) {
- I2C_SendData(I2C1, buffer[i]);
- while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
- }
- I2C_GenerateSTOP(I2C1, ENABLE);
+	NAND_ADDRESS WriteReadAddr;
+
+	WriteReadAddr.Zone = zone;
+	WriteReadAddr.Block = block;
+	WriteReadAddr.Page = page;
+
+	return WriteReadAddr;
 }
-/**
- * @brief Read data from memory.
- * @author Jakub Brzezowski
- * @version 1.2 15-01-2019
- * @param addr is address, from data was written.
- * @param data is place, where read data, was written.
- * @param size is size of data.
- * @retval none
- */
-void eeprom_read(uint32_t addr, void* data, int size)
-{
- int i;
- uint8_t* buffer = (uint8_t*)data;
 
- eeprom_set_addr(addr);
+void write_buffer(uint8_t* buffer, NAND_ADDRESS WriteReadAddr, uint8_t page) {
 
- I2C_GenerateSTART(I2C1, ENABLE);
- while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS);
+	FSMC_NANDDeInit(FSMC_Bank2_NAND);
+	FSMC_NAND_Init();
 
- I2C_AcknowledgeConfig(I2C1, ENABLE);
- I2C_Send7bitAddress(I2C1, 0xa0, I2C_Direction_Receiver);
- while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS);
+	FSMC_NAND_EraseBlock(WriteReadAddr);
+	FSMC_NAND_WriteSmallPage(buffer, WriteReadAddr, page);
 
- for (i = 0; i < size - 1; i++) {
- while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS);
-     buffer[i] = I2C_ReceiveData(I2C1);
+}
+
+void read_buffer(uint8_t* buffer, NAND_ADDRESS WriteReadAddr, uint8_t page) {
+
+	FSMC_NANDDeInit(FSMC_Bank2_NAND);
+	FSMC_NAND_Init();
+
+	FSMC_NAND_WriteSmallPage(buffer, WriteReadAddr, page);
+
+}
+void data_from_pc_in_memory_write(uint16_t zone, uint16_t block, uint16_t page) {
+
+	int buffer[256];
+	data_from_pc_read(*buffer);
+
+	NAND_IDTypeDef NAND_ID;
+	NAND_ADDRESS WriteReadAddr = write_read_address_set(zone, block, page);
+
+}
     }
  I2C_AcknowledgeConfig(I2C1, DISABLE);
     I2C_GenerateSTOP(I2C1, ENABLE);
